74929d23659bc3c70fb47312ebe609c2b2ee7c05
tolluset <dlqud19@gmail.com>
2023-09-24 02:49:23 +0900

    feat: add timer hooks

---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useTimer({ remainedTime }: { remainedTime: number; period?: number }) {
+  const [time, setTime] = useState(remainedTime);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(time => time - 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  function secondsToHms(seconds: number) {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
+    return [h, m, s].map(v => (v < 10 ? "0" + v : v)).join(":");
+  }
+
+  return secondsToHms(time);
+}
