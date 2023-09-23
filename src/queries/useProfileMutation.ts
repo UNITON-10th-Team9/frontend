e2473e0b152884cfc9df4629a08bf73dbd6a51f3
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-23 22:00:02 +0900

    rename: httClient -> httpClient

--- src/queries/useProfileMutation.ts
+++ src/queries/useProfileMutation.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "react-query";
 import { Profile } from "@/types/profile";
-import httpClient from "@/services/httClient";
+import httpClient from "@/services/httpClient";
 
 const fetch = async (params: Profile & { phoneNumber: string }) => {
   const { data } = await httpClient.post("/users", params);
